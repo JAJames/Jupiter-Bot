a7a1fda51ef7bd992eca966ac964c478e8a61ffd
JustinAJ <justin.aj@hotmail.com>
2014-10-17 01:13:09 -0400

    Corrected copyright notice.

--- RenX.DNS/RenX_DNS.cpp
+++ RenX.DNS/RenX_DNS.cpp
@@ -1,8 +1,17 @@
 /**
- * This file is in the public domain, furnished "as is", without technical
- * support, and with no warranty, express or implied, as to its usefulness for
- * any purpose.
+ * Copyright (C) 2014 Justin James.
  *
+ * This license must be preserved.
+ * Any applications, libraries, or code which make any use of any
+ * component of this program must not be commercial, unless explicit
+ * permission is granted from the original author. The use of this
+ * program for non-profit purposes is permitted.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * In the event that this license restricts you from making desired use of this program, contact the original author.
  * Written by Justin James <justin.aj@hotmail.com>
  */
 
