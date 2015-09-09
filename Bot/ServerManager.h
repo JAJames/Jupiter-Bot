dd4800c881f7afec6f88fd4023593e50a14a3e4a
JustinAJ <justin.aj@hotmail.com>
2015-09-08 23:55:59 -0400

    Licensing change; now using ISC license. Added LICENSE file

--- Bot/ServerManager.h
+++ Bot/ServerManager.h
@@ -1,17 +1,18 @@
 /**
  * Copyright (C) 2014 Justin James.
  *
- * This license must be preserved.
- * Any applications, libraries, or code which make any use of any
- * component of this program must not be commercial, unless explicit
- * permission is granted from the original author. The use of this
- * program for non-profit purposes is permitted.
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  *
- * In the event that this license restricts you from making desired use of this program, contact the original author.
  * Written by Justin James <justin.aj@hotmail.com>
  */
 
