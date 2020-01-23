e72fa2486d4f17ffc7b6ee5f7003bc7f33dd568e
Jessica James <jessica.aj@outlook.com>
2020-01-23 12:50:53 -0600

    Added RenX.KickDupes Updated Jupiter version

---
+++ src/Plugins/RenX/RenX.KickDupes/RenX_KickDupes.cpp
@@ -0,0 +1,58 @@
+/**
+ * Copyright (C) 2020 Jessica James.
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ * OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
+ * CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ *
+ * Written by Jessica James <jessica.aj@outlook.com>
+ */
+
+#include "RenX_Core.h"
+#include "RenX_Server.h"
+#include "RenX_Functions.h"
+#include "RenX_PlayerInfo.h"
+#include "RenX_KickDupes.h"
+
+using namespace Jupiter::literals;
+
+bool RenX_KickDupesPlugin::initialize() {
+	return true;
+}
+
+void RenX_KickDupesPlugin::RenX_OnPlayerIdentify(RenX::Server &in_server, const RenX::PlayerInfo &in_player) {
+	// Safety checks
+	if (in_player.hwid.isEmpty()) {
+		// Somehow, the player's identified without any HWID. TODO: log this
+		return;
+	}
+
+	// Exempt development servers from this check
+	if (in_server.getGameVersion().findi("-DEV"_jrs) != Jupiter::INVALID_INDEX) {
+		return;
+	}
+
+	// Check to see if any other players on the server have the same HWID
+	for (auto& player : in_server.players) {
+		if (player.hwid == in_player.hwid && player.id != in_player.id) {
+			// Two players have the same HWID, but separate player IDs; kick the pre-existing player.
+			in_server.forceKickPlayer(player, "Ghost client detected"_jrs);
+		}
+	}
+}
+
+// Plugin instantiation and entry point.
+RenX_KickDupesPlugin pluginInstance;
+
+extern "C" JUPITER_EXPORT Jupiter::Plugin *getPlugin()
+{
+	return &pluginInstance;
+}
