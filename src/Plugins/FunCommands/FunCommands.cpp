8883a05637f0f9d30e17eeb96674b424846a6b29
Jessica James <jessica.aj@outlook.com>
2021-12-08 17:05:52 -0600

    Update Jupiter modile to hopefully resolve compilation issues on specific platforms; Mass-update copyright headers based on files touched this year

--- src/Plugins/FunCommands/FunCommands.cpp
+++ src/Plugins/FunCommands/FunCommands.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2014-2015 Jessica James.
+ * Copyright (C) 2014-2021 Jessica James.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
--- src/Plugins/RenX/RenX.Ladder.Monthly/RenX_Ladder_Monthly.cpp
+++ src/Plugins/RenX/RenX.Ladder.Monthly/RenX_Ladder_Monthly.cpp
@@ -1,5 +1,5 @@
 /**
- * Copyright (C) 2016-2017 Jessica James.
+ * Copyright (C) 2016-2021 Jessica James.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
